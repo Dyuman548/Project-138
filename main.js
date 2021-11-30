c7d17dc593cd93b96364a43f0ad3ac64e75f9e39
Dyuman548 <73636626+Dyuman548@users.noreply.github.com>
2021-11-30 13:05:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,26 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-}
+  
+    canvas = createCanvas(1240,336);
+    canvas.parent('canvas');
+  
+    instializeInSetup(mario);
+  
+    video = createCapture(VIDEO);
+    video.size(800,400);
+    video.parent('game_console');
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  
+  }
+  
+  function modelLoaded() 
+  {
+    console.log('Model Loaded!');
+  }  
+
 
 
 function draw(){
